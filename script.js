835e3b60919f66f5e0e1e5a1a1ed3916c5706b17
DustyMick <damicklo5@gmail.com>
2020-12-30 10:15:32 -0600

    pass prompt options added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+function generatePassword() {
+var choosePassLength = prompt("How long would you like your password?")  
+var chooseLowerCase = confirm("Would you like lower case")
+var chooseUpperCase = confirm("Would you like upper case")
+var chooseNumber = confirm("would you like numbers")
+var chooseSpecialCharacter = confirm("Would you like special characters")
 
+var lowerCaseArray = "abcdefghijklmnopqrstuvwxyz" .split ("")
+ 
+var upperCaseArray = "ABCDEFGHIJKLMNOPQRSTUYWXYZ" .split ("")
+
+var numberArray = "0123456789" . split ("")
+
+var specialCharacterArray = "!&*$#{}=+@()?" .split ("")
+
+var userChosenArray = []
+   if (chooseLowerCase) { 
+   console.log(chooseLowerCase)
+   }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
